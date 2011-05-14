adf589fd37b1dbe2a6283f148b86ffe13654cd4e
Scott Wichser <blast007@users.sourceforge.net>
2011-05-14 14:19:29 +0000

    Turn off the server key check until we implement -publickey in BZFlag 2.3
    git-svn-id: https://svn.code.sf.net/p/bzflag/code/trunk/db@21427 08b3d480-bf2c-0410-a26f-811ee3361c24

--- bzfls.php
+++ bzfls.php
@@ -728,6 +728,7 @@ function action_add() {
   $ownerID = "";
   
   // check the server key
+/* Commented out until we decide if 2.3/2.4 will require a server key
   if ($version != 'BZFS0026' && $version != 'BZFS1910')
   {
 	$result = mysql_query("SELECT host, owner FROM authkeys WHERE key_string='" . mysql_real_escape_string($serverKey) . "'");
@@ -757,6 +758,7 @@ function action_add() {
 		return;	
 	}
   }
+*/
 
   # Filter out badly formatted or buggy versions
   print "MSG: ADD $nameport $version $gameinfo " . stripslashes($slashtitle) . "\n";
